db19cf6a726fffe4c77c3dc8d0d7aa679be5e980
Elton Minetto <eminetto@gmail.com>
2017-09-30 11:05:00 -0300

    Salvando uma cerveja

--- aula1/restbeer/index.php
+++ aula1/restbeer/index.php
@@ -36,6 +36,28 @@ $app->get('/beer/{id}', function ($request, $response, $next) use ($beers) {
     return $response;
 });
 
+$db = new PDO('sqlite:beers.db');
+$app->post('/beer', function ($request, $response, $next) use ($db) {
+    $db->exec(
+        "create table if not exists 
+beer (id INTEGER PRIMARY KEY AUTOINCREMENT, name text not null, style text not null)"
+    );
+
+    $data = $request->getParsedBody();
+    //@TODO: clean form data before insert into the database ;)
+    $stmt = $db->prepare('insert into beer (name, style) values (:name, :style)');
+    $stmt->bindParam(':name',$data['name']);
+    $stmt->bindParam(':style', $data['style']);
+    $stmt->execute();
+    $data['id'] = $db->lastInsertId();
+    if ($data['id'] == 0) {
+        return $response->withStatus(500, "Erro salvando cerveja");
+    }
+    $response->getBody()->write($data['id']);
+
+    return $response->withStatus(201);
+});
+
 
 $app->pipeRoutingMiddleware();
 $app->pipeDispatchMiddleware();
