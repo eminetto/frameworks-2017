214ffc72d48c16951a217e1f383a6ca0c442b001
Elton Minetto <eminetto@gmail.com>
2017-09-30 10:36:20 -0300

    Mostrar todas

--- aula1/restbeer/index.php
+++ aula1/restbeer/index.php
@@ -10,6 +10,22 @@ $app->get('/', function ($request, $response, $next) {
     return $response;
 });
 
+$beers = [
+    'brands' => ['Heineken', 'Guinness', 'Skol', 'Colorado'],
+    'styles' => ['Pilsen' , 'Stout']
+];
+
+$app->get('/brands', function ($request, $response, $next) use ($beers) {
+    $response->getBody()->write(implode(',', $beers['brands']));
+    return $response;
+});
+
+$app->get('/styles', function ($request, $response, $next) use ($beers) {
+    $response->getBody()->write(implode(',', $beers['styles']));
+    return $response;
+});
+
+
 $app->pipeRoutingMiddleware();
 $app->pipeDispatchMiddleware();
 $app->run();
