6966a8db58a1caa7e21d2b8176d1b2dad6fd1d95
Hygor Zorak <hygorzorak@gmail.com>
2017-10-07 08:47:27 -0300

    changing route file

--- aula1/restbeer/src/routes.php
+++ aula1/restbeer/src/routes.php
@@ -5,9 +5,9 @@ $db = new \PDO('sqlite:beers.db');
 //managing the application routes
 
 $app->get('/', new \RestBeer\Http\Home());
+$app->post('/login', new \RestBeer\GenerateToken());
 $app->get('/brands', new \RestBeer\Http\NewBrand());
 $app->get('/styles', new \RestBeer\Http\Styles());
-$app->get('/beer/{id}', new \RestBeer\Http\Beer());
 $app->post('/beer', new \RestBeer\Http\NewBeer($db));
+$app->get('/beer/{id}', new \RestBeer\Http\Beer());
 $app->put('/beer/{id}', new \RestBeer\Http\UpdateBeer($db));
-$app->post('/login', new \RestBeer\GenerateToken());
