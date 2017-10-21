c24458322b34abb6ffd4aa8a31ab7f21f50fbf17
Elton Minetto <eminetto@gmail.com>
2017-10-21 11:54:41 -0200

    Controllers

--- aula3/restbeer/module/Application/config/module.config.php
+++ aula3/restbeer/module/Application/config/module.config.php
@@ -34,11 +34,44 @@ return [
                     ],
                 ],
             ],
+            // 'beer_1' => [
+            //     'type'    => Segment::class,
+            //     'options' => [
+            //         'route'    => '/beer[/][:id]',
+            //         'constraints' => [
+            //             'id'     => '[0-9]+',
+            //         ],
+            //         'defaults' => [
+            //             'controller' => Controller\BeerController::class,
+            //             'action'     => 'get',
+            //         ],
+            //     ],
+            // ],
+            'beer' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route'    => '/beer[/][:action][/:id]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\BeerController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
         ],
     ],
     'controllers' => [
         'factories' => [
             Controller\IndexController::class => InvokableFactory::class,
+            Controller\BeerController::class => function(\Interop\Container\ContainerInterface $container, $requestedName) {
+                $tableGateway = $container->get('Application\Model\BeerTableGateway');
+                $controller = new Controller\BeerController($tableGateway);
+
+                return $controller;
+            },
         ],
     ],
     'view_manager' => [
